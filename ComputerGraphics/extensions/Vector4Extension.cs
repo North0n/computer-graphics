48e8dcc8804ccedb5944c80737fe6c027e7e3706
North0n <mautin.ivan.3@gmail.com>
2023-11-26 21:52:56 +0300

    add ToVector3 method extension

---
+++ ComputerGraphics/extensions/Vector4Extension.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace ComputerGraphics.extensions;
+
+public static class Vector4Extension
+{
+    public static Vector3 ToVector3(this Vector4 vec)
+    {
+        return new Vector3(vec.X, vec.Y, vec.Z);
+    }
+}
